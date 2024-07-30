9a0d7d93d6a269d06a05e24e32e6a94a7d2aa9ca
lekhana-13 <166687011+lekhana-13@users.noreply.github.com>
2024-07-30 20:55:23 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,23 @@
+public class User {
+    private String userId;
+    private String pin;
+    private Account account;
+
+    public User(String userId, String pin, Account account) {
+        this.userId = userId;
+        this.pin = pin;
+        this.account = account;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
